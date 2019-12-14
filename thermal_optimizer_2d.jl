634fa557df50fd1034967d54a76717af48b010fe
Zachary Zumbo <zumbo@mit.edu>
2019-12-14 17:31:28 -0500

    Sad zack finds out implementing convolution in 2D takes only 3 lines, after struggling for over an hour

--- thermal_optimizer_2d.jl
+++ thermal_optimizer_2d.jl
@@ -3,13 +3,17 @@ using LinearAlgebra
 using Plots
 using ForwardDiff
 using ProgressMeter
-using DiffEqOperators
+using DSP
 
 N = 4
 A = zeros(N,N)
 
 x_size = 10
-x = zeros(10,10)
+x = reshape(collect(1.0:100.0),(10,10))
+
+stencil = [0.0  1.0 0.0; 
+           1.0 -4.0 1.0; 
+           0.0  1.0 0.0]
 
 # generate_u_prime(x, dx)
 
@@ -32,32 +36,14 @@ end
 function double_partial(x)
     dx = zeros(size(x))
     for i in 2:length(dx)-1
-      dx[i] = -1*x[i-1] + 2*x[i] + -1*x[i+1]
+      dx[i] = 1*x[i-1] - 2*x[i] + 1*x[i+1]
     end
-    dx[1] = (2*x[1] + -1*x[2])
-    dx[end] = (-1*x[end-1] + 2*x[end])
+    dx[1] = -(2*x[1] + -1*x[2])
+    dx[end] = -(-1*x[end-1] + 2*x[end])
 end
 
 function double_partial_2D(x)
-
-
+    padded_du = conv2(x, stencil)
+    du = padded_du[2:end-1,2:end-1]
+    du
 end
-
-dx = 1.0
-domain = RegularGrid((0,5),dx)
-# is a regular grid of [0,5] with spacing dx. But now we have to start talking about boundaries. It's the domain that knows what boundaries it has, and thus what has to be specified. So to the domain we need to add boundary functions.
-
-# Boundary conditions are specified by types <:AbstractBoundaryCondition. So we could have:
-
-lbc = Dirichlet(0)
-rbc = Neumann(g(t,x))
-# Now we'd package that together:
-
-space = Space(domain,lbc,rbc)
-# Those domains are compatible with those boundary conditions, so it works without error. Once we have a space, our Heat Equation is specified via:
-
-u' = D(t,x,u)*A*u + f(t,x,u)
-# so we specify:
-
-tspan = (0,15)
-prob = HeatProblem(D,f,space,tspan)
